7f7dbeed28197fa3260342612da1414201f5f04b
zark <wenbo.zhang@duitang.com>
2018-02-06 17:52:23 +0800

    add upload

--- src/components/upload.vue
+++ src/components/upload.vue
@@ -1,14 +1,12 @@
 <template>
-<el-upload
-  action="https://wepic.dtxn.net/api/v1/anonymous/images/"
-  list-type="picture-card"
-  :on-preview="handlePictureCardPreview"
-  :on-remove="handleRemove">
-  <i class="el-icon-plus"></i>
-</el-upload>
-<el-dialog :visible.sync="dialogVisible">
-  <img width="100%" :src="dialogImageUrl" alt="">
-</el-dialog>
+  <el-main>
+    <el-upload action="https://wepic.dtxn.net/api/v1/anonymous/images/" list-type="picture-card" :on-preview="handlePictureCardPreview" :on-remove="handleRemove">
+      <i class="el-icon-plus"></i>
+    </el-upload>
+    <el-dialog :visible.sync="dialogVisible">
+      <img width="100%" :src="dialogImageUrl" alt="">
+    </el-dialog>
+  </el-main>
 </template>
 
 <script>
